9ad03f3da8f13b730f77d7ff41b171070e987391
Himanshu Kotnala <himanshu.kotnala99@gmail.com>
2020-08-21 14:59:17 +0530

    Placeholder fixed, bottom adv improved

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,26 @@
     }
   });
 
-  $('footer').hover(
-    () => $('.banner-add').fadeOut('slow'), 
-    () => $('.banner-add').fadeIn('slow')
-  );
+  // $('footer').hover(
+  //   () => $('.banner-add').fadeOut('slow'), 
+  //   () => $('.banner-add').fadeIn('slow')
+  // );
+  var isScrolledIntoView = function(elem) {
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  $(window).on('scroll', function () {
+      $('#adv1').toggle(!isScrolledIntoView('#footer'));
+  });
 
   if ($(window).scrollTop() > 100) {
     $('#header').addClass('header-scrolled');
